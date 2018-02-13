fe45e4c6f3d644ca691012b24e2982025f8d0b53
Andy Hadjigeorgiou <ahh2131@columbia.edu>
2018-02-13 15:07:31 -0500

    Move callback

--- modules/better_off_alone.js
+++ modules/better_off_alone.js
@@ -8,13 +8,13 @@ exports.handle = function(sender, pieces, db, callback) {
     .play(better_off_alone)
     .then(success => {
       console.log("Talk to me");
+      callback({
+        message: "Talk to me ..."
+      });
     })
     .catch(err => {
       console.log("Error occurred %j", err);
     });
-  callback({
-    message: "Talk to me ..."
-  });
   // Useful to find Eng Sonos IP address again if necessary...
   // const { DeviceDiscovery } = require("sonos");
   // const search = Sonos.DeviceDiscovery({ timeout: 30000 });
