473e7dbeefe5e55d89a74912efe88d946eae1bcb
Ryan Young <ryoung786@gmail.com>
2018-02-15 16:47:00 -0500

    statsd test

--- modules/echo.js
+++ modules/echo.js
@@ -1,3 +1,6 @@
+const statsd = require("../lib/statsd");
+
 exports.handle = function(sender, pieces, storageFactory, callback) {
+  statsd.increment("pair_programming.foo.bar");
   callback({ message: pieces.join(" ") });
 };
